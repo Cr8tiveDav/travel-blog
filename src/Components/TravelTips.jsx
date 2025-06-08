52b3b44aca07ca30ca2dc41f30f4aaec2e09d33d
Cr8tiveDav <114363324+Cr8tiveDav@users.noreply.github.com>
2025-06-08 22:35:45 +0100

    feat: integrate contentful for CMS

--- src/Components/TravelTips.jsx
+++ src/Components/TravelTips.jsx
@@ -3,77 +3,40 @@ import tipImg2 from '../assets/images/Travel-bag.jpeg';
 import tipImg3 from '../assets/images/Travel-app.jpeg';
 import tipImg4 from '../assets/images/Travel-scam.jpeg';
 import { Link } from 'react-router-dom';
+import { useFetchBlogs } from '../CustomHooks/useFetchBlogs';
 
 const TravelTips = () => {
+  const { isLoading, blogs } = useFetchBlogs();
+  console.log(isLoading);
+  console.log(blogs);
   return (
     <section id='travelTips' className='align-element py-8'>
       <h2 className='text-2xl sm:text-3xl font-semibold text-cyan-600 text-center mb-8'>
         Travel Tips
       </h2>
-      <div className='grid gap-8 w-max mx-auto text-gray-800 sm:grid-cols-2 lg:grid-cols-3'>
-        <Link
-          to='/blog'
-          className='w-75 sm:w-70 md:w-80 min-h-70 p-4 bg-neutral-50 border-1 border-slate-300 rounded-xl cursor-pointer active:scale-102 active:bg-cyan-50 hover:scale-102 hover:bg-slate-100 transform transition-all duration-300'
-        >
-          <div className='h-52'>
-            <img
-              src={tipImg1}
-              alt='World travel'
-              className='w-full h-full object-cover rounded-lg'
-            />
-          </div>
-          <h3 className='text-lg leading-5 font-bold mt-4'>
-            Top 10 Things to Do Before Your First International Trip
-          </h3>
-        </Link>
-
-        <Link
-          to='/blog'
-          className='w-75 sm:w-70 md:w-80 min-h-70 p-4 bg-neutral-50 border-1 border-slate-300 rounded-xl cursor-pointer active:scale-102 active:bg-cyan-50 hover:scale-102 hover:bg-slate-100 transform transition-all duration-300'
-        >
-          <div className='h-52'>
-            <img
-              src={tipImg2}
-              alt='Traveling bag'
-              className='w-full h-full object-cover rounded-lg'
-            />
-          </div>
-          <h3 className='text-lg leading-5 font-bold mt-4'>
-            How to Pack Smart: The Art of Traveling
-          </h3>
-        </Link>
-
-        <Link
-          to='/blog'
-          className='w-75 sm:w-70 md:w-80 min-h-70 p-4 bg-neutral-50 border-1 border-slate-300 rounded-xl cursor-pointer active:scale-102 active:bg-cyan-50 hover:scale-102 hover:bg-slate-100 transform transition-all duration-300'
-        >
-          <div className='h-52'>
-            <img
-              src={tipImg3}
-              alt='Travel app'
-              className='w-full h-full object-cover rounded-lg'
-            />
-          </div>
-          <h3 className='text-lg leading-5 font-bold mt-4'>
-            Essential Travel Apps Every Traveler Should Download
-          </h3>
-        </Link>
-
-        <Link
-          to='/blog'
-          className='w-75 sm:w-70 md:w-80 min-h-70 p-4 bg-neutral-50 border-1 border-slate-300 rounded-xl cursor-pointer active:scale-102 active:bg-cyan-50 hover:scale-102 hover:bg-slate-100 transform transition-all duration-300'
-        >
-          <div className='h-52'>
-            <img
-              src={tipImg4}
-              alt='Travel warning'
-              className='w-full h-full object-cover rounded-lg'
-            />
-          </div>
-          <h3 className='text-lg leading-5 font-bold mt-4'>
-            Travel Scams Around the World - and How to Avoid Them
-          </h3>
-        </Link>
+      <div
+        className='grid gap-8 w-max mx-auto text-gray-800
+      sm:grid-cols-2 lg:grid-cols-3'
+      >
+        {blogs.map((blog) => {
+          const { id, title, img } = blog;
+          return (
+            <Link
+              key={id}
+              to='/blog'
+              className='w-75 sm:w-70 md:w-80 min-h-70 p-4 bg-neutral-50 border-1 border-slate-300 rounded-xl cursor-pointer active:scale-102 active:bg-cyan-50 hover:scale-102 hover:bg-slate-100 transform transition-all duration-300'
+            >
+              <div className='h-52'>
+                <img
+                  src={img}
+                  alt={title}
+                  className='w-full h-full object-cover rounded-lg'
+                />
+              </div>
+              <h3 className='text-lg leading-5 font-bold mt-4'>{title}</h3>
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
--- src/CustomHooks/useFetchBlogs.jsx
+++ src/CustomHooks/useFetchBlogs.jsx
@@ -1,5 +1,5 @@
 import { createClient } from 'contentful';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const client = createClient({
   space: import.meta.env.VITE_SPACE_ID,
@@ -7,21 +7,33 @@ const client = createClient({
   accessToken: import.meta.env.VITE_API_KEY,
 });
 
-console.log(import.meta.env.VITE_API_KEY);
-
-const useFetchBlogs = () => {
+export const useFetchBlogs = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const [blogs, setBlogs] = useState([]);
 
   const getData = async () => {
     try {
       const resp = await client.getEntries({
-        content_type: 'Travel Blog Post',
+        content_type: 'travelBlogPost',
       });
+
+      const data = resp.items.map((blog) => {
+        const { title, images } = blog.fields;
+        const id = blog.sys.id;
+        const img = images[0]?.fields?.file?.url;
+        return { title, id, img };
+      });
+      setIsLoading(false);
+      setBlogs(data);
     } catch (error) {
+      setIsLoading(false);
       console.log(error);
     }
   };
 
-  return <div>useFetchBlogs</div>;
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return { isLoading, blogs };
 };
-export default useFetchBlogs;
